2abeeb071669d5c7ec1f1b4e07e13d9fa2121cfe
mummanavasanthi <153973630+mummanavasanthi@users.noreply.github.com>
2026-05-20 12:35:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import StudentCard from "./components/StudentCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Component Reuse Example</h1>
+
+      <StudentCard
+        name="Vasanthi"
+        course="Computer Science"
+        college="ABC College"
+      />
+
+      <StudentCard
+        name="Rahul"
+        course="Electronics"
+        college="XYZ College"
+      />
+
+      <StudentCard
+        name="Priya"
+        course="Mechanical"
+        college="PQR College"
+      />
     </div>
   );
 }
